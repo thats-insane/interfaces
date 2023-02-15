e3ec8f5ffd77748c22ac4ea06f6b9b88a69f9811
krismic <2021154337@ub.edu.bz>
2023-02-15 13:31:41 -0600

    reverse added

--- main.go
+++ main.go
@@ -6,15 +6,27 @@ import (
 	"strings"
 )
 
+func reverse(char string) string {
+	var result string
+	for _, value := range char {
+		result = string(value) + result
+	}
+
+	return result
+}
+
 func main() {
 	msg := flag.String("msg", "sup", "the message to be deisplayed")
 	num := flag.Int("num", 1, "how much times to print the message")
 	caps := flag.Bool("u", false, "uppercase")
-
+	revrse := flag.Bool("r", false, "reverse")
 	flag.Parse()
 
 	if *caps {
 		*msg = strings.ToUpper(*msg)
+	}
+	if *revrse {
+		*msg = reverse(*msg)
 	}
 
 	for i := 0; i < *num; i++ {
